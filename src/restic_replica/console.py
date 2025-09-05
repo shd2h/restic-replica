0e1d8640c143ec940829809a276d6d57a71f8d2c
shd2h <nathan.aj.cronin@gmail.com>
2025-09-05 12:25:56 +0100

    return configured logger instance

--- src/restic_replica/console.py
+++ src/restic_replica/console.py
@@ -7,20 +7,41 @@ from typing import Optional
 logger = logging.getLogger(__name__)
 
 
-def setup_logging(logdir: Optional[Path] = None) -> None:
-    if not logdir:
-        logdir = Path.home() / ".restic-replica"
-    timestamp = datetime.now().isoformat(timespec="seconds")
-    logname = f"restic-replica_{timestamp}.log"
-    # create logging dir
-    logdir.mkdir(parents=True, exist_ok=True)
-    logging.basicConfig(
-        level=logging.INFO,
-        handlers=[
-            # logging.FileHandler("{0}/{1}".format(logdir, logname)),
-            logging.StreamHandler(sys.stdout),
-        ],
-    )
+def setup_logging(
+    logger: logging.Logger = logging.getLogger("restic_replica"),
+    logdir: Optional[Path] = Path.home() / ".restic-replica",
+    debug: bool = False,
+) -> logging.Logger:
+
+    # set debugging log level if specified
+    if debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    # # setup formatting
+    # formatter = logging.Formatter(
+    #     fmt="%(asctime)s %(levelname)s: %(message)s",
+    #     datefmt="%Y-%m-%d %H:%M:%SZ",
+    # )
+
+    # setup logging to console
+    ch = logging.StreamHandler()
+    # ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    # setup logging to file
+    if logdir:
+        timestamp = datetime.now().isoformat(timespec="seconds")
+        logname = f"restic-replica_{timestamp}.log"
+        # create logging dir
+        logdir.mkdir(parents=True, exist_ok=True)
+        fh = logging.FileHandler("{0}/{1}".format(logdir, logname))
+        # fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    # return the logger object
+    return logger
 
 
 def logging_headers(version: str) -> None:
