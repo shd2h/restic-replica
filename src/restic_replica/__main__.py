6ea3a3b429b689043ba5347618281a818817b7df
shd2h <nathan.aj.cronin@gmail.com>
2025-09-09 13:10:56 +0100

    core program loop implemented

--- src/restic_replica/__main__.py
+++ src/restic_replica/__main__.py
@@ -2,9 +2,24 @@ from restic_replica import __version__, app, console
 
 
 def main():
-    config = app.read_config_file()
-    console.setup_logging()
-    app.logging_headers(__version__)
+    config_file = app.ensure_config_file()
+    config = app.read_config_file(config_file)
+    # TODO: read the logging path from the config file. Allow setting to none?
+    logger = console.setup_logging()
+    console.logging_headers(__version__)
+    # get restic cli
+    restic_cli = app.get_restic(config["restic"])
+    # instance source and target repositories
+    source = app.get_repository("source", config["source"], restic_cli)
+    destination = app.get_repository("target", config["destination"], restic_cli)
+    # check access, then trigger copy
+    try:
+        app.check_repository_access(source)
+        app.check_repository_access(destination)
+        app.copy_snapshots(source, destination)
+    except RuntimeError as err:
+        logger.error(err)
+        raise SystemExit(1)
 
 
 if __name__ == "__main__":
